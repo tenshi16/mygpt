97956d6cee5670a8a1bdd3f2a5867f8269bb8288
Angel Gomez <tenshigomez@gmail.com>
2024-03-03 21:48:27 -0300

    fix: proper size of text bubble icon

--- components/ChatBubble/ChatBubble.tsx
+++ components/ChatBubble/ChatBubble.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Flex, Text, rem } from '@mantine/core';
+import { Flex, Text, rem, Grid } from '@mantine/core';
 import { IconBrandMantine } from '@tabler/icons-react';
 interface Bubble {
   isGPT: boolean,
@@ -9,17 +9,21 @@ interface Bubble {
 }
 const ChatBubble = ({isGPT, message, name}: Bubble) => {
   return (
-    <Flex gap="md">
+    <Grid gutter="xs">
+      <Grid.Col span="content">
       <IconBrandMantine
         style={{ width: rem(30), height: rem(30) }}
         stroke={1.5}
         color="var(--mantine-color-blue-filled)"
       />
+      </Grid.Col>
+      <Grid.Col span={11}>
       <Flex direction="column">
         <Text size="md" fw={700}>{name}</Text>
         <Text size="md">{message}</Text>
       </Flex>
-    </Flex>
+      </Grid.Col>
+    </Grid>
   );
 }
 
--- components/ChatContainer/ChatContainer.tsx
+++ components/ChatContainer/ChatContainer.tsx
@@ -13,7 +13,18 @@ const MOCK_CHATS = [{
     message: `
 Sure! Imagine you have a shape that's made up of smaller versions of itself, and those smaller versions are made up of even smaller versions, and so on, repeating forever. That's basically what a fractal is.
 Fractals are shapes that repeat themselves at smaller and smaller scales, no matter how much you zoom in. They're like never-ending patterns, where each part looks like the whole thing, just smaller. Think of a tree branch that splits into smaller branches, and those smaller branches split again, and so on, creating a pattern that looks similar at every level of magnification. That's a simple way to understand fractals!
-`}]
+`},
+  {
+    isGPT: false,
+    name: 'Angel',
+    message: 'explain fractals as simply as possible'
+  }, {
+    isGPT: true, 
+    name: 'My GPT',
+    message: `
+    That's a simple way to understand fractals!
+`}
+]
 const ChatContainer = () => {
   return (
     <Stack gap="xl">
